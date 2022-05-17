c8ca8fb770483b855a06871c51fcfba7f45ed5a3
ranjithvutnoor <ranjithvutnoor@gmail.com>
2022-05-17 23:38:21 +0530

    first commit

---
+++ madlib.py
@@ -0,0 +1,19 @@
+animal_name1 = input('animal_name1: ')
+animal_name2 = input('animal_name1: ')
+adj1= input('Adjective1: ')
+adj2= input('Adjective2: ')
+adj3= input('Adjective3: ')
+adj4 = input('adjective4: ')
+adv1= input('adverb: ')
+adv2 = input('adv2: ')
+verb1 = input('verb1: ')
+verb2 = input('verb2: ')
+verb3 = input('verb2: ')
+
+madlib = f'This is an {adv1} {adj1} story about a {animal_name1} and a {animal_name2}.\
+The hare is an animal that is known to move {adj2}, while a tortoise is one to move {adj3}.\
+One day, the hare {verb1} the tortoise to a {verb2} simply to prove that he was the best. The tortoise agreed.\
+Once the race began the hare was {verb3} able to get a head start. Upon realizing that the tortoise is far behind. The overconfident hare decided to take a nap.\
+Meanwhile the tortoise, who was extremely determined and dedicated to the race was slowly nearing the finish line.\
+The tortoise won the race while the hare {adv2}. Most importantly he did it with {adj4} and without arrogance.' 
+print(madlib)
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Mad Libs (a play on ad lib, from Latin ad libitum - as you wish) 
+is a word game where one player prompts another for a list of words to substitute for blanks in a story;
+these word substitutions have a humorous effect when the resulting story is then read aloud.
